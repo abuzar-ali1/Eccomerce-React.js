d48e30c97bb3c145c30a6884066d1a4a80675934
Abuzar Ali <abuzaralixwave@gmail.com>
2024-12-15 22:35:32 +0500

    part-2

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignIn from "./Components/auth/SignIn/SignIn";
 import SignUp from "./Components/auth/SignUp/Signup";
 import ErrorPage from "./Components/Error/ErrorPage";
+import ProductDetails from "./Components/product-details/ProductDetails";
 function App() {
   const router = createBrowserRouter([
     { path: "/", element: <Appbar />, errorElement: <ErrorPage/> },
+    {path: "/product-Details/:product-id", element: <ProductDetails/> },
     { path: "/Sign-In", element: <SignIn/> , errorElement: <ErrorPage/> },
     { path: "/Sign-Up", element: <SignUp/>, errorElement: <ErrorPage/>  },
   ]);
--- src/Components/products/productCards.jsx
+++ src/Components/products/productCards.jsx
@@ -1,35 +1,43 @@
-import { Box, Button, Card, Rating, Typography } from "@mui/material";
-import React from "react";
+import { Box, Button, Card, Grid, Rating, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
+import axios from "axios";
 
-const ProductCard = (props) => {
-  const { product } = props;
+const ProductCard = () => {
+  const [products, setProducts] = useState();
+  console.log(products);
+  useEffect(() => {
+    const productData = axios
+      .get("https://api.escuelajs.co/api/v1/products")
+      .then((data) => setProducts(data.data));
+  }, []);
+
   return (
-    <Box className="d-flex gap-2">
-      {product.map((porductList) => {
+    <Grid container>
+      {products?.map((porduct) => {
         return (
-     
-              <Card className="p-2">
-                <img src={porductList.img} alt="" />
-                <Typography variant="body2">{porductList.category}</Typography>
-                <h6>{porductList.name}</h6>
-                <Rating
-                  size="small"
-                  name="read-only"
-                  value={porductList.rating}
-                  readOnly
-                />
-                <Box className="d-flex justify-content-between align-items-center">
-                  <Typography>{porductList.price}</Typography>
-                  <Button size="small" variant="contained">
-                    <AddIcon /> Add
-                  </Button>
-                </Box>
-              </Card>
-       
+          <Grid item sm={3}>
+            <Card className="p-2">
+              <img className="img-fluid" src={porduct?.images[0]} alt="" />
+              <Typography variant="body2">{porduct?.category?.name}</Typography>
+              <h6>{porduct?.title}</h6>
+              <Rating
+                size="small"
+                name="read-only"
+                value={porduct?.rating}
+                readOnly
+              />
+              <Box className="d-flex justify-content-between align-items-center">
+                <Typography>{porduct?.price}</Typography>
+                <Button size="small" variant="contained">
+                  <AddIcon /> Add
+                </Button>
+              </Box>
+            </Card>
+          </Grid>
         );
       })}
-    </Box>
+    </Grid>
   );
 };
 
